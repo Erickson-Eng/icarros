f54d044f92080f84f826a90f3afad6f6696d8c90
Erickson Azevedo <erickson.tulio96@gmail.com>
2022-02-13 17:43:35 -0300

    feat: in-memory database configuration for testing

--- backend/src/main/java/com/icarros/backend/application/config/SwaggerConfig.java
+++ backend/src/main/java/com/icarros/backend/application/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.icarros.backend.application.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +21,18 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.icarros.backend.application.controller"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Aplicação de consumo Icarros")
+                .description("Teste para desenvolvedor Java")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("Erickson Azevedo", "https://github.com/Erickson-Eng/icarros", "erickson.tulio96@gmail.com"))
                 .build();
     }
 }
