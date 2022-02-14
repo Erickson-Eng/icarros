daa208dc905047201c64cc1e7f626e6631dd9fea
Erickson Azevedo <erickson.tulio96@gmail.com>
2022-02-14 17:16:01 -0300

    feat(MAJOR): creating custom exceptions

--- backend/src/main/java/com/icarros/backend/application/exception/NotFoundException.java
+++ backend/src/main/java/com/icarros/backend/application/exception/NotFoundException.java
@@ -1,2 +1,10 @@
-package com.icarros.backend.application.exception;public class NotFoundException {
+package com.icarros.backend.application.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException{
+
+
 }
